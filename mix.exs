7416b40fec972f26bfa4a466041a5c173e4d4b01
Kip Cole <kipcole9@gmail.com>
2022-06-08 10:53:01 +0800

    Fix SPDX licence

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule Cldr.Calendars.Persian.MixProject do
   defp package do
     [
       maintainers: ["Kip Cole"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       logo: "logo.png",
       links: links(),
       files: [
